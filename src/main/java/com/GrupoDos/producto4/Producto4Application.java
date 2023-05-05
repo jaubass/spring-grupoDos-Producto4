ed519dd067b2a048c1c200af5a24f7c8d2428ad0
Jau <jaubass@gmail.com>
2023-05-05 22:01:08 +0200

    GET y POST

--- src/main/java/com/GrupoDos/producto4/Producto4Application.java
+++ src/main/java/com/GrupoDos/producto4/Producto4Application.java
@@ -13,8 +13,11 @@ public class Producto4Application {
 		ApplicationContext context = SpringApplication.run(Producto4Application.class, args);
 		UserRepository repository = context.getBean(UserRepository.class);
 
-		User user1 = new User(null, "Marcus", "marcus@mail.com", "1234", "777666555", "NY", "08987");
+		User user1 = new User(null, "Marcus", "marcus@uoc.com", "1234", "777666555", "NY", "08987");
+		User user2 = new User(null, "Jau", "jau@uoc.com", "12", "123456789", "BCN", "08016");
+
 		repository.save(user1);
+		repository.save(user2);
 		System.out.println(repository.findAll().size());
 		// repository.deleteById(1L);
 
