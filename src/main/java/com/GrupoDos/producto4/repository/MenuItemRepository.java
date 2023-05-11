93be0caf2853f56844c0a03fbd8cd69a8a3a93fb
xavi-mat <1102677+xavi-mat@users.noreply.github.com>
2023-05-11 19:27:33 +0200

    clean all

--- src/main/java/com/GrupoDos/producto4/repository/MenuItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.GrupoDos.producto4.repository;
-
-import com.GrupoDos.producto4.model.MenuItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
-}
--- src/main/java/com/GrupoDos/producto4/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.GrupoDos.producto4.repository;
-
-import com.GrupoDos.producto4.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
